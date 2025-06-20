fa35566bb8b6a13f500dffcbf305cb69e0d22aff
AndrianTiana200745 <andrian7iana@gmail.com>
2025-06-20 11:39:22 +0300

    Update reservation.sql

--- resource/reservation.sql
+++ resource/reservation.sql
@@ -9,7 +9,7 @@ CREATE TABLE Utilisateur (
     nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    mot_de_passe VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'utilisateur') DEFAULT 'utilisateur',
     date_creation DATETIME DEFAULT CURRENT_TIMESTAMP
 );
